47d9ae2a99636f560938eac5f5d31f114ee2c9b3
pas2704 <pas2704@outlook.com>
2022-09-13 21:19:54 -0400

    Add project files.

---
+++ AutoFactionChat/AutoFactionChatComponent.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using Sandbox.Game.GameSystems;
+using Sandbox.Game.Gui;
+using System.Reflection;
+
+namespace AutoFactionChat
+{
+    [HarmonyPatch]
+    internal static class MyChatSystemPatch
+    {
+        private static MethodBase TargetMethod()
+        {
+            return AccessTools.Constructor(typeof(MyChatSystem));
+        }
+
+        private static void Postfix(ref ChatChannel ___m_currentChannel)
+        {
+            ___m_currentChannel = ChatChannel.Faction;
+            Plugin.Log.Info("Switched to Faction Channel");
+        }
+    }
+}
---
+++ AutoFactionChat/Plugin.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using NLog;
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using VRage.Plugins;
+
+namespace AutoFactionChat
+{
+    public class Plugin : IPlugin, IDisposable
+    {
+        public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void Init(object gameInstance)
+        {
+            new Harmony("AutoFactionChat").PatchAll(Assembly.GetExecutingAssembly());
+            Log.Info($"AutoFactionChat loaded");
+        }
+        public void Dispose()
+        {
+        }
+        public void Update()
+        {
+        }
+    }
+}
